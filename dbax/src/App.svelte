a29075a1bc1bd5d91938d72117ad8e42b621c873
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-08-12 11:56:03 +0100

    learnt event forwarding

--- dbax/src/App.svelte
+++ dbax/src/App.svelte
@@ -1,6 +1,9 @@
 <script>
+	import Modal from'./Modal.svelte'
 	let name = 'dbax';
 	let age =18;
+
+	let showModal = false;
 
 	const handleClick = () => {
 		age += 1;
@@ -9,9 +12,37 @@
 	const handleInput = (e) => {
 		age = e.target.value;
 	}
+
+	let people = [
+		{name:'dbax',beltColour: 'black', age:18, id:1},
+		{name:'fere', beltColour:'red',age:12,id:2},
+		{name:'ebun',beltColour:'white',age:16,id:4}
+	]
+
+	const handleDelete = (id) => {
+		// console.log(id)
+		//if this returns true, the element will be kept in the array
+		//if it returns false, the element will be removed from the array
+		people = people.filter((person) => person.id != id);
+		//delete a person from people
+	}
+	const toggleModal = () => {
+		showModal = !showModal
+	}
+	let num = 15;
 </script>
 
+<!-- {#if num > 20}
+	<p>the number is greater than 20</p>
+{:else if num > 5}
+	<p>greater than 5</p>
+{:else}
+	<p>not greater than 5</p>
+{/if} -->
+
+<Modal message="my first svelte prop" isPromo={true} showModal={showModal} on:click={toggleModal} />
 <main>
+	<button on:click={toggleModal}>Open Modal</button>
 	<h1>Hello {name}!</h1>
 	<p>Welcome to my domain</p>
 	<p style="font-size: {age}px">You are {age} years old</p>
@@ -20,6 +51,19 @@
 	 <input type="text" bind:value={age} />
 	 <!-- this bind keyword does the twoway binding all at once -->
 	<button on:click={handleClick}>update age</button>
+	<!-- this is to loop through a given array -->
+	{#each people as person (person.id)}
+	<div>
+		<h4>{person.name}</h4>
+		{#if person.beltColour === 'black'}
+			<p>Master Ninja</p>
+		{/if}
+		<p>{person.age} years old, {person.beltColour} belt.</p>
+		<button on:click={() => {handleDelete(person.id)}}>delete</button>
+	</div>
+	{:else}
+	<p>There are no people</p>
+	{/each}
 </main>
 
 <style>
---
+++ dbax/src/Modal.svelte
@@ -0,0 +1,40 @@
+<script>
+    //for variables that get their values from outside, you export them first
+    export let message = 'Im the default value';
+    export let showModal = false
+    export let isPromo = false
+</script>
+
+
+{#if showModal}
+<!-- this will assign a promo class if what is in the curly braces evaluates to true -->
+ <!-- this is called event forwarding -->
+    <div class="backdrop" class:promo={isPromo} on:click>
+        <div class="modal">
+            <p>{message}</p>
+        </div>
+    </div>
+{/if}
+
+
+<style>
+.backdrop {
+   width: 100%;
+   height: 100%;
+   position: fixed;
+   background: rgba(0,0,0,0.8); 
+}
+
+.modal {
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;
+    text-align: center;
+    background: white;
+}
+.promo .modal{
+    background: crimson;
+    color: white;
+}
+</style>
