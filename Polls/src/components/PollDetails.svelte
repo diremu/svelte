773ba61fabc27798efad880ca4e8136ff9b89600
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-08-19 14:27:47 +0100

    use tweened to adjust the vote transition

--- Polls/src/components/PollDetails.svelte
+++ Polls/src/components/PollDetails.svelte
@@ -2,13 +2,23 @@
   import Card from "../shared/Card.svelte";
   import PollStore from "../stores/PollStore";
   import Button from '../shared/Button.svelte'
+  import { tweened } from "svelte/motion"
   export let poll;
 
 //   const dispatch = createEventDispatcher();
   //reactive value
   $: totalVotes = poll.votesA + poll.votesB;
-  $: percentA = Math.floor((100 / totalVotes) * poll.votesA);
-  $: percentB = Math.floor((100 / totalVotes) * poll.votesB);
+  $: percentA = Math.floor((100 / totalVotes) * poll.votesA) || 0;
+  $: percentB = Math.floor((100 / totalVotes) * poll.votesB) || 0;
+  //the 0 is for when there are no votes yet
+
+  const tweenedA = tweened(0)
+  const tweenedB = tweened(0)
+    //to update the tweened value whenever the other reactive value changes
+  $: tweenedA.set(percentA)
+  $: tweenedB.set(percentB)
+
+//   $: console.log($tweenedA, $tweenedB)
 
   const handleVote = (answer, id) => {
     PollStore.update((currentPolls) => {
@@ -42,13 +52,13 @@
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote("a", poll.id)}>
       <!-- to show the percentage of votes for the current answer -->
-      <div class="percent percent-a" style="width: {percentA}%"></div>
+      <div class="percent percent-a" style="width: {$tweenedA}%"></div>
       <!-- dynamically styles based on current value -->
       <span>{poll.answerA} ({poll.votesA})</span>
     </div>
     <!-- svelte-ignore a11y-click-events-have-key-events -->
     <div class="answer" on:click={() => handleVote("b", poll.id)}>
-      <div class="percent percent-b" style="width: {percentB}%"></div>
+      <div class="percent percent-b" style="width: {$tweenedB}%"></div>
       <span>{poll.answerB} ({poll.votesB})</span>
     </div>
     <div class="delete">
