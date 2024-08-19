9068c4b1b25f39049623ab295458e77682c93710
Diremu Adebanjo <diremuadebanjo@gmail.com>
2024-08-19 12:49:03 +0100

    lifecycle hooks

--- Polls/src/components/PollList.svelte
+++ Polls/src/components/PollList.svelte
@@ -1,16 +1,31 @@
 <script>
     import PollStore from "../stores/PollStore.js";
     import PollDetails from "./PollDetails.svelte";
+    // import { onMount, onDestroy } from 'svelte';
     export let polls = []; 
 
     //the subscribe method takes in the data from the store passed as a parameter
-    PollStore.subscribe((data) => {
-        polls = data;
-    })
+    // const unsub = PollStore.subscribe((data) => {
+    //     polls = data;
+    // })
+    // onMount(() => {
+    //     //more useful to fetch data from an API before the page is rendered
+    //     console.log('mounted')
+    // })
+
+    // onDestroy(() => {
+    //     console.log('destroyed')
+    //     unsub()
+    //     //unsubscribes from the store
+    // })
 </script>
 
+<!-- this will make the pollstore subs and unsubs
+    only useful if you want to simply use the data from the store
+    otherwise, you use the onMount and onDestroy methods
+-->
 <div class="poll-list">
-    {#each polls as poll (poll.id)}
+    {#each $PollStore as poll (poll.id)}
         <div>
             <!-- this forwards the event to the parent element -->
             <PollDetails {poll} on:vote />
